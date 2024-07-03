21a0474b73f024eeea8a6a0e02bf2b028342b880
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-07-03 23:09:24 +0530

    Navbar

--- data/index.ts
+++ data/index.ts
@@ -4,7 +4,6 @@ export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
     { name: "Resume", link: "https://avs-resume.tiiny.site/"},
-    { name: "Coursework", link: "#coursewok" },
     { name: "Contact", link: "#contact" },
   ];
   
